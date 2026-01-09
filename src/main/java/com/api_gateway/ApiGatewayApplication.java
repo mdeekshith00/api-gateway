9954a94768f70e3ec0d0edc4c1662012d381e494
mdeekshith00 <mdeekshith000@gmail.com>
2026-01-10 00:36:39 +0530

    excluded jpa classes

--- src/main/java/com/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/api_gateway/ApiGatewayApplication.java
@@ -2,11 +2,16 @@ package com.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @EnableDiscoveryClient
-@SpringBootApplication(scanBasePackages = {"com.api_gateway","com.common"})
+@SpringBootApplication(   exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+    } , scanBasePackages = {"com.api_gateway","com.common"})
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
